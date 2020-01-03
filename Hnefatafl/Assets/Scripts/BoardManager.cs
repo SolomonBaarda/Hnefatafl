c2642a6120ec1b2d701205bc67a00fe56a87d8be
SolomonBaarda <solomon_baarda@outlook.com>
2020-01-03 12:32:08 +0000

    Added piece movement

--- Hnefatafl/Assets/Scripts/BoardManager.cs
+++ Hnefatafl/Assets/Scripts/BoardManager.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
+
+    public static BoardManager Instance { set; get; }
+    private bool[,] allowedMoves { set; get; }
+
+    public Piece[,] Board { set; get; }
+    private Piece selectedPiece;
+
     private const float TILE_SIZE = 1.0f;
     private const float TILE_OFFSET = 0.5f;
-    private const int BOARD_SIZE = 9;
+    public const int BOARD_SIZE = 9;
 
     private int selectionX = -1;
     private int selectionY = -1;
@@ -13,14 +20,85 @@ public class BoardManager : MonoBehaviour {
     public List<GameObject> gamePiecePrefabs;
     private List<GameObject> activePieces;
 
+    public bool isAttackingTurn = true;
+
     private void Start()
     {
+        Instance = this;
         SpawnAllPieces();
     }
 
     private void Update () {
         UpdateTileSelection();
         DrawBoard ();
+
+        // Left click
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(selectionX >= 0 && selectionY >= 0)
+            {
+                if(selectedPiece == null)
+                {
+                    // Select the new piece
+                    SelectPiece(selectionX, selectionY);
+                }
+                else
+                {
+                    // Move the piece
+                    MovePiece(selectionX, selectionY);
+                }
+            }
+        }
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        // Not a valid selection 
+        if(Board[x,y] == null)
+        {
+            return;
+        }
+        if(Board[x,y].isAttacking != isAttackingTurn)
+        {
+            return;
+        }
+
+        allowedMoves = Board[x, y].PossibleMove();
+        selectedPiece = Board[x, y];
+        BoardHighlight.Instance.HighlightAllowedMoves(allowedMoves);
+    }
+
+    private void MovePiece(int x, int y)
+    {
+        if(allowedMoves[x,y])
+        {
+            Piece p = Board[x, y];
+
+            if(p != null && p.isAttacking != isAttackingTurn)
+            {
+
+                // Is the king?
+                if(p.GetType() == typeof(King))
+                {
+                    // End the game
+                    return;
+                }
+
+                // Capture a piece 
+                activePieces.Remove(p.gameObject);
+                Destroy(gameObject.gameObject);
+            }
+
+            Board[selectedPiece.CurrentX, selectedPiece.CurrentY] = null;
+            selectedPiece.transform.position = GetTileCentre(x, y);
+            selectedPiece.SetPosition(x, y);
+            Board[x, y] = selectedPiece;
+
+            isAttackingTurn = !isAttackingTurn;
+        }
+
+        BoardHighlight.Instance.HideHighlights();
+        selectedPiece = null;
     }
 
     private void SpawnAllPieces()
@@ -28,18 +106,31 @@ public class BoardManager : MonoBehaviour {
         // Index 0: defending, 1: attacking, 2: king
         activePieces = new List<GameObject>(); activePieces = new List<GameObject>();
 
-        // Spawn the black pieces
-        SpawnPiece(0, 0, 4);
-        SpawnPiece(0, 4, 0);
-        SpawnPiece(0, 4, 8);
-        SpawnPiece(0, 8, 4);
+        Board = new Piece[BOARD_SIZE, BOARD_SIZE];
 
+        // Spawn the black pieces
+        SpawnPiece(0, 0, 3);
+        SpawnPiece(0, 0, 4);
+        SpawnPiece(0, 0, 5);
+        SpawnPiece(0, 3, 0);
+        SpawnPiece(0, 4, 0);
+        SpawnPiece(0, 5, 0);
+        SpawnPiece(0, 3, 8);
+        SpawnPiece(0, 4, 8);
+        SpawnPiece(0, 5, 8);
+        SpawnPiece(0, 8, 3); 
+        SpawnPiece(0, 8, 4);
+        SpawnPiece(0, 8, 5);
 
         // Spawn the white pieces
+        SpawnPiece(1, 3, 3);
         SpawnPiece(1, 3, 4);
+        SpawnPiece(1, 3, 5);
         SpawnPiece(1, 4, 3);
-        SpawnPiece(1, 5, 4);
         SpawnPiece(1, 4, 5);
+        SpawnPiece(1, 5, 3);
+        SpawnPiece(1, 5, 4);
+        SpawnPiece(1, 5, 5);
 
         // Spawn the king 
         SpawnPiece(2, 4, 4);
@@ -49,14 +140,18 @@ public class BoardManager : MonoBehaviour {
     {
         GameObject go = Instantiate(gamePiecePrefabs[index], GetTileCentre(x,y), Quaternion.identity) as GameObject;
         go.transform.SetParent(transform);
+
+        Board[x, y] = go.GetComponent<Piece>();
+        Board[x, y].SetPosition(x, y);
+
         activePieces.Add(go);
     }
 
-    private Vector3 GetTileCentre(int x, int y)
+    private Vector3 GetTileCentre(int tileX, int tileY)
     {
         Vector3 origin = Vector3.zero;
-        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
-        origin.z += (TILE_SIZE * y) + TILE_OFFSET;
+        origin.x += (TILE_SIZE * tileX) + TILE_OFFSET;
+        origin.z += (TILE_SIZE * tileY) + TILE_OFFSET;
 
         return origin;
     }
---
+++ Hnefatafl/Assets/Scripts/Piece.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public int CurrentX { set; get; }
+    public int CurrentY { set; get; }
+
+    // Value to check determine the team of the piece
+    public bool isAttacking;
+
+
+    public void SetPosition(int x, int y)
+    {
+        this.CurrentX = x;
+        this.CurrentY = y;
+    }
+
+
+    public bool[,] PossibleMove()
+    {
+        // Array of valid moves for the piece 
+        bool[,] r = new bool[BoardManager.BOARD_SIZE, BoardManager.BOARD_SIZE];
+
+        // Temp values
+        Piece p;
+        int i;
+
+        // Moving right
+        i = CurrentX;
+        while (true)
+        {
+            i++;
+            if (i >= BoardManager.BOARD_SIZE)
+            {
+                break;
+            }
+            p = BoardManager.Instance.Board[i, CurrentY];
+            if (p == null)
+            {
+                r[i, CurrentY] = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Move left
+        i = CurrentX;
+        while (true)
+        {
+            i--;
+            if (i < 0)
+            {
+                break;
+            }
+            p = BoardManager.Instance.Board[i, CurrentY];
+            if (p == null)
+            {
+                r[i, CurrentY] = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Move up
+        i = CurrentY;
+        while (true)
+        {
+            i++;
+            if (i >= BoardManager.BOARD_SIZE)
+            {
+                break;
+            }
+            p = BoardManager.Instance.Board[CurrentX, i];
+            if (p == null)
+            {
+                r[CurrentX, i] = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Move down
+        i = CurrentY;
+        while (true)
+        {
+            i--;
+            if (i < 0)
+            {
+                break;
+            }
+            p = BoardManager.Instance.Board[CurrentX, i];
+            if (p == null)
+            {
+                r[CurrentX, i] = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+
+        return r;
+    }
+
+}
