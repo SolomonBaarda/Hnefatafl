4da1daf887d142cae7cd45233d8e5e3ecfb38a6e
Solomon Baarda <50365603+SolomonBaarda@users.noreply.github.com>
2020-01-15 14:15:10 +0000

    Added basic main menu

---
+++ Hnefatafl/Assets/Scripts/Menu/Play.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void PlayHnefatafl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayTablut()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
